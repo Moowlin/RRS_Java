d0bd557323c16a2d949e3b8750929641d454c782
Olga Nizhegorodtseva <mo.owlinn@gmail.com>
2024-09-26 12:38:38 +0300

    start homework 5

--- src/RRS/Homework_6.java
+++ src/RRS/Homework_6.java
@@ -2,5 +2,32 @@ package RRS;
 
 public class Homework_6 {
     public void displayInfo() {
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("-----------------------------= HOMEWORK 6 =-----------------------------");
+        System.out.println("-------------------------------= Task 1 =-------------------------------");
+        System.out.println("""
+                Условие:
+                Необходимо создать класс Employee со следующими методами:\s
+                getBaseSalary - получить базовую ставку
+                setBaseSalary
+                getName - получить имя
+                setName
+                getSalary - получить зарплату
+                """);
+
+        class Person{
+            String name;
+            int age;
+            void displayInfoPerson(){
+                System.out.printf("Name: %s \tAge: %d\n", name, age);
+            }
+        }
+
+        Person tom = new Person();
+        tom.displayInfoPerson();
+
+        tom.name = "Tom";
+        tom.age = 18;
+        tom.displayInfoPerson();
     }
 }
