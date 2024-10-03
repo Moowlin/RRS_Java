d3be93d034f0009068cdd77c484e1dd6aaf9f039
Olga Nizhegorodtseva <mo.owlinn@gmail.com>
2024-10-03 14:25:01 +0300

    finished director class

--- src/RRS/Homework6/Director.java
+++ src/RRS/Homework6/Director.java
@@ -1,4 +1,18 @@
 package RRS.Homework6;
 
-public class Director {
+public class Director extends Manager {
+
+    public Director(String name, String position, double baseSalary, int numberOfSubordinates) {
+        super(name, position, baseSalary, numberOfSubordinates);
+    }
+
+    @Override
+    public double getSalary() {
+        if (numberOfSubordinates > 0) {
+            this.salary = this.baseSalary * (this.numberOfSubordinates / 100 * 9);
+        } else {
+            this.salary = this.baseSalary;
+        }
+        return salary;
+    }
 }
