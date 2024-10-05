d7b4210563080ac23e9f1cb073bef0721f09efd0
Moowlin <mo.owlinn@gmail.com>
2024-10-05 15:00:45 +0300

    проблему с выводм текста решила, теперь необходимо отредактировать файл Start.menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import RRS.Start;
-
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static RRS.Start.getUserResponse;
@@ -9,27 +7,20 @@ import static RRS.Start.getUserResponse;
 public class Main {
     private static final String FILENAME = "resources/messages";
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(FILENAME);
-
-    
     public static void main(String[] args) {
-        System.out.println("test");
-        String rb = resourceBundle.getString("greeting");
-        System.out.println(rb);
-        
+        System.out.println(resourceBundle.getString("greeting"));
 
+        boolean start = Start.selectTask();
+        String userResponse = "";
 
-//        //System.out.println(ResourceBundle.getBundle("messages", current).getString("greeting"));
-//        boolean start = Start.selectTask();
-//        String userResponse = "";
-//
-//        while (!userResponse.equals("0")){
-//            System.out.println(ResourceBundle.getBundle("main_menu"));
-//            userResponse = getUserResponse();
-//            //start(userResponse);
-//        }
-//
-//        if (start){
-//            System.out.println(ResourceBundle.getBundle("goodbye"));
-//        }
+        while (!userResponse.equals("0")){
+            System.out.println(resourceBundle.getString("main_menu"));
+            userResponse = getUserResponse();
+            //start(userResponse);
+        }
+
+        if (start){
+            System.out.println(resourceBundle.getString("goodbye"));
+        }
     }
 }
